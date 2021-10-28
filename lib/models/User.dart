7e1f174347154bad22171061b20e73701550e430
pavan-aeturi <f20180212@goa.bits-pilani.ac.in>
2021-10-28 18:31:58 +0530

    add login integration

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -1,24 +1,23 @@
 class User {
   final String email;
+  String userId, name, userAuthToken;
+  bool isActive, isStaff;
 
-  final String name;
-
-  bool isActive;
-
-  bool isStaff;
-
-  User(this.name, this.email);
+  User(this.email);
 
   User.fromJson(Map<String, dynamic> json)
       : name = json['name'],
         email = json['email'],
-        isActive = json['is_active'],
-        isStaff = json['is_staff'];
+        isActive = json['is_active'] == 'true' ? true : false,
+        isStaff = json['is_staff'] == 'true' ? true : false,
+        userId = json['user_id'],
+        userAuthToken = json['token'];
 
   Map<String, dynamic> toJson() => {
         'name': name,
         'email': email,
         'is_active': isActive,
-        'is_staff': isStaff
+        'is_staff': isStaff,
+        'user_id': userId,
       };
 }
--- lib/models/UserManager.dart
+++ lib/models/UserManager.dart
@@ -1,17 +1,26 @@
+import 'dart:async';
 import 'dart:convert';
-
 import 'User.dart';
 import 'package:http/http.dart' as http;
 
 class UserManager {
-  static Future<http.Response> createUser(String name, String email) {
-    return http.post(
+  static Future<User> createUser(
+      String name, String email, String password) async {
+    final response = await http.post(
       Uri.parse('http://127.0.0.1:8000/api/user'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
-      body: jsonEncode(new User(name, email).toJson()),
+      body: jsonEncode(new User(email).toJson()),
     );
+    if (response.statusCode == 201) {
+      User user = new User(email);
+      user.name = name;
+
+      return user;
+    } else {
+      throw Exception('Failed to create user.');
+    }
   }
 
   static Future<User> getUser(String email) async {
@@ -25,7 +34,28 @@ class UserManager {
     if (response.statusCode == 200) {
       return User.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to create album.');
+      throw Exception('Failed to get user.');
+    }
+  }
+
+  static Future<User> login(String email, String password) async {
+    final response = await http.post(
+        Uri.parse('http://127.0.0.1:8000/api/login/'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: <String, String>{
+          'username': email,
+          'password': password
+        });
+
+    if (response.statusCode == 200) {
+      User user = new User(email);
+      user.userId = jsonDecode(response.body)['user_id'];
+      user.userAuthToken = jsonDecode(response.body)['token'];
+      return user;
+    } else {
+      throw Exception('Failed to login.');
     }
   }
 }
